c85a11f1da49623a6a8879c7bc714f51906adefa
Matt Gauger <matt.gauger@gmail.com>
2018-11-25 18:55:11 -0600

    Progress from 2018-11-24

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const Artifact = require('./classes/artifact')
-const Character = require('./classes/character')
-const Language = require('./classes/language')
-
 const _ = require('lodash')
 const seaduck = require('seaduck')
 const tracery = require('tracery-grammar')
 const fs = require('fs')
+
+const Artifact = require('./classes/artifact')
+const Character = require('./classes/character')
+const Language = require('./classes/language')
+
+// Data files:
+const lastNames = JSON.parse(fs.readFileSync('corpora/data/humans/lastNames.json', 'utf8'))["lastNames"]
+const adjs = JSON.parse(fs.readFileSync('corpora/data/words/adjs.json', 'utf8'))["adjs"]
+const bodyParts = JSON.parse(fs.readFileSync('corpora/data/humans/bodyParts.json', 'utf8'))["bodyParts"]
 
 const grammarJson = JSON.parse(fs.readFileSync('./grammar.json', 'utf8'))
 let gibberishGrammar = tracery.createGrammar(grammarJson)
@@ -29,13 +34,42 @@ function getGibberish() {
 // 10. Generate the final chapter
 // 11. Draw PDF
 
+let worldLanguage = new Language({
+  minsyll: 1,
+  maxsyll: 3,
+  structure: 'CV',
+  //C: "ptkmnls",
+  V: "aeiouU"
+});
+worldLanguage.langName = `Ancient ${worldLanguage.langName}`
+
+const bookTitle = `The Placeholder of ${worldLanguage.makeName()}`
+console.log(bookTitle);
+console.log(`A history written in ${worldLanguage.langName}.\n`);
+
 let chapters = []
 
+// Entity system:
+// - Characters persist through various chapters (regions)
+// - Regions get built for each chapter, and they contain objects, enemies, etc.
 let entities = []
 
-entities.push(new Character({tags: ['character', 'child'], properties: {age: _.random(4,6)}}));
-entities.push(new Character({tags: ['character', 'child'], properties: {age: _.random(7,10)}}));
-entities.push(new Character({tags: ['character', 'child'], properties: {age: _.random(11,13)}}));
+let childrenLastName = _.sample(lastNames)
+
+entities.push(new Character({
+  tags: ['character', 'child'],
+  properties: {age: _.random(4,6), lastName: childrenLastName}
+}));
+
+entities.push(new Character({
+  tags: ['character', 'child'],
+  properties: {age: _.random(7,10), lastName: childrenLastName}
+}));
+
+entities.push(new Character({
+  tags: ['character', 'child'],
+  properties: {age: _.random(11,13), lastName: childrenLastName}
+}));
 
 let wizardNames = ['Hilde', 'Noirin', 'Butterflax', 'Anise', 'Matilda']
 entities.push(new Character({
@@ -47,36 +81,54 @@ entities.push(new Character({
   }
 }));
 
-let squirrelLang = new Language({
-  minsyll: 1,
-  maxsyll: 2,
-  V: "aeiou",
-  vortho: {},
-  cortho: {}
-});
+//let squirrelLang = new Language({
+  //minsyll: 1,
+  //maxsyll: 2,
+  //V: "aeiou",
+  //vortho: {},
+  //cortho: {}
+//});
 
 let squirrel = new Character({
-  name: squirrelLang.makeName(),
+  name: worldLanguage.makeName(),
   tags: ['character'],
   properties: {
     race: 'squirrel person',
     age: _.random(1,10),
     languages: {
-      spoken: [squirrelLang.langName],
-      understood: [squirrelLang.langName],
-      read: [squirrelLang.langName]
+      spoken: [worldLanguage.langName],
+      understood: [worldLanguage.langName],
+      read: [worldLanguage.langName]
     }
   }
 })
-//console.log(squirrel);
-//console.log(squirrel.properties.languages);
 entities.push(squirrel)
 
 // initial entities + world state
 //console.log(entities);
 
 // Introduction
-chapters.push({title: 'Introduction', text: 'Once upon a time'})
+let introText = ''
+
+let wizardName = entities[3].name
+let squirrelName = entities[4].name
+
+function adj() {
+  return _.sample(adjs);
+}
+
+introText += `The ${childrenLastName} children were all asleep in their beds. As you may already know, sleep is when the world of imagination is closest to our world. That is why, at this moment, a ${adj()} ${_.sample(bodyParts)} appeared in the middle of the room. There was a ripping sound, like a pair of trousers being split, and then the owner of that body part stepped fully through the background. It was, of course, ${wizardName}. But you will meet them later. The hole they left behind seemed to be in the wall itself, as if they'd stepped in from the next room. But the room behind the hole was ${adj()} and ${adj()}, completely unlike the children's bedroom.\n\n`
+
+introText += `Sneaking in behind ${wizardName} was a peculiar sight. A small woodland creature, in a ${adj()} jumpsuit. It turned, revealing that it was carrying a small backpack with various leaves sticking out of it. Its tiny eyes darted nervously around the room.\n\n`
+
+let sayWords = getGibberish().replace(/"+/gi, '').replace(/[ ]+/, ' ')
+introText += `"${sayWords}", it chirped.\n\n`
+
+introText +=`"That may well be, ${squirrelName}", said ${wizardName}. "But I am sure this is the right place!"\n\n`
+
+introText += `${wizardName} snapped their fingers, and the ${childrenLastName} disappeared out of their beds. Turning, ${wizardName} and ${squirrelName} disappeared back through the hole in time and space. A needle and thread appeared, sewing up the gateway, which faded until it disappeared completely.`
+
+chapters.push({title: 'Introduction', text: introText})
 
 // In each region:
 // 1. Introduce how the characters got there.
@@ -118,42 +170,27 @@ let n = new seaduck.Narrative({
     {
       "match": ["#character"],
       "when": function(a) {
-        return a.properties.sleepiness == 7;
+        return a.properties.race
       },
       "action": function*(a) {
-        yield new seaduck.StoryEvent("reallySleepy", a);
+        yield new seaduck.StoryEvent("describeRace", a);
       }
     },
-    {
-      "match": ["#character", "#thing"],
-      "when": function(a, b) {
-        return a.properties.sleepiness >= 10 
-          && !this.isRelated("sleepingIn", a, b)
-          && !b.properties.occupied;
-      },
-      "action": function*(a, b) {
-        this.relate("sleepingIn", a, b);
-        b.properties.occupied = true;
-        yield new seaduck.StoryEvent("getsInto", a, b);
-      }
-    },
-    {
-      "match": ["Chris", "king-size bed"],
-      "when": function(a, b) {
-        return this.isRelated("sleepingIn", a, b);
-      },
-      "action": function*(a, b) {
-        yield new seaduck.StoryEvent("asleep", a, b);
-      }
-    }
   ],
   "initialize": function*() {
-    yield new seaduck.StoryEvent('describe', this.noun('house of the wizard'));
+    yield new seaduck.StoryEvent('describeRoom', this.noun('house of the wizard'));
+    //yield new seaduck.StoryEvent('describePerson', this.noun(entities[0].name));
+    //yield new seaduck.StoryEvent('describePerson', this.noun(entities[1].name));
+    //yield new seaduck.StoryEvent('describePerson', this.noun(entities[2].name));
   },
   "traceryDiscourse": {
     "amiable": ["#nounA# is a friendly sort of person.", "#nounA# is often good-natured."],
     "isAlive": ["#nounA# is alive."],
-    "describe": ["The #nounA# is where everyone is."],
+    "describeRace": ["#nounA# is a #nounA_race#."],
+    "smell1": ["cheeze", "old socks", "baloney"],
+    "smell2": ["lilac", "earth", "wood"],
+    "describeRoom": ["The #nounA# is where everyone is. It smells like #smell1# and #smell2# at once."],
+    "describePerson": ["#nounA# is #nounA_age# years old."],
     "_end": ["That's all."]
   }
 })
@@ -162,7 +199,6 @@ let strs = []
 let step = 0;
 let stepsDesired = 25;
 
-//for (let i = 0; i < maxSteps; i++) {
 while (step < stepsDesired) {
   let storyEvents = n.stepAndRender();
   if (storyEvents.length > 0) {
@@ -177,7 +213,8 @@ while (step < stepsDesired) {
 }
 str = _.uniq(strs).join('\n')
 
-chapters.push({title: 'Chapter #!', text: str})
+let chapterNum = 1
+chapters.push({title: `Chapter ${chapterNum}.`, text: str})
 
 // Ending
 chapters.push({title: '', text: `Then the squirrel person ${squirrel.name} said: '${getGibberish()}' and it was over.\n\nTHE END`})
